e331831fa6e4641176cb35053a79533b98d197d0
nsf <no.smile.face@gmail.com>
2011-10-22 19:01:22 +0600

    Use closures aggressively in the example, remove global vars.

--- examples/colors.go
+++ examples/colors.go
@@ -2,33 +2,27 @@ package main
 
 import "fmt"
 import "tk"
-import "os"
-
-var entryText *tk.StringVar
-var ir *tk.Interpreter
-var RGB [3]int
-
-func sayHi() {
-	ir.Eval(`.label configure -text [subst -nocommands -novariables {` + entryText.Get() + `}]`)
-	entryText.Set("")
-}
 
 func main() {
-	var err os.Error
-	ir, err = tk.NewInterpreter()
+	ir, err := tk.NewInterpreter()
 	if err != nil {
 		panic(err)
 	}
 
-	entryText = ir.NewStringVar("entryText")
-	ir.RegisterCallback("sayHi", sayHi)
+	entryText := ir.NewStringVar("entryText")
+	ir.RegisterCallback("updateLabel", func() {
+		ir.Eval(`.label configure -text [subst -nocommands -novariables {` + entryText.Get() + `}]`)
+		entryText.Set("")
+	})
+
+	var RGB [3]int
 	ir.RegisterCallback("scaleUpdate", func(idx int, x float64) {
 		RGB[idx] = int(x)
 		col := fmt.Sprintf("%02X%02X%02X", RGB[0], RGB[1], RGB[2])
 		ir.Eval(`.label configure -foreground #` + col)
 	})
 
-	ir.Eval(`ttk::button .hello -text "Press me!" -command sayHi`)
+	ir.Eval(`ttk::button .hello -text "Press me!" -command updateLabel`)
 	ir.Eval(`ttk::entry .entry -textvariable entryText`)
 	ir.Eval(`ttk::label .label -text "Press a button"`)
 	ir.Eval(`.label configure -foreground #FF0000`)
