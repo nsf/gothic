c38e842a854e725f149d7b5ec6cbd0b1e6f23380
nsf <no.smile.face@gmail.com>
2011-10-24 22:17:17 +0600

    Simplify colors example.

--- examples/colors.go
+++ examples/colors.go
@@ -9,12 +9,6 @@ func main() {
 		panic(err)
 	}
 
-	entryText := ir.NewStringVar("entryText")
-	ir.RegisterCallback("updateLabel", func() {
-		ir.Eval(`.label configure -text {` + entryText.Get() + `}`)
-		entryText.Set("")
-	})
-
 	var RGB [3]int
 	ir.RegisterCallback("scaleUpdate", func(idx int, x float64) {
 		if RGB[idx] == int(x) {
@@ -22,15 +16,18 @@ func main() {
 		}
 		RGB[idx] = int(x)
 		col := fmt.Sprintf("%02X%02X%02X", RGB[0], RGB[1], RGB[2])
-		ir.Eval(`.label configure -foreground #` + col)
+		ir.Eval(`ttk::style configure My.TFrame -background #` + col)
+		ir.Eval(`.frame configure -style My.TFrame`)
 	})
 
-	ir.Eval(`ttk::button .hello -text "Press me!" -command updateLabel`)
-	ir.Eval(`ttk::entry .entry -textvariable entryText`)
-	ir.Eval(`ttk::label .label -text "Press a button"`)
-	ir.Eval(`ttk::scale .scaleR -from 0 -to 255 -length 200 -command {scaleUpdate 0}`)
-	ir.Eval(`ttk::scale .scaleG -from 0 -to 255 -length 200 -command {scaleUpdate 1}`)
-	ir.Eval(`ttk::scale .scaleB -from 0 -to 255 -length 200 -command {scaleUpdate 2}`)
-	ir.Eval(`pack .hello .entry .label .scaleR .scaleG .scaleB`)
+	ir.Eval(`
+ttk::frame .frame -width 100 -height 30 -relief sunken
+
+ttk::scale .scaleR -from 0 -to 255 -length 200 -command {scaleUpdate 0}
+ttk::scale .scaleG -from 0 -to 255 -length 200 -command {scaleUpdate 1}
+ttk::scale .scaleB -from 0 -to 255 -length 200 -command {scaleUpdate 2}
+pack .frame -fill both
+pack .scaleR .scaleG .scaleB
+	`)
 	ir.MainLoop()
 }
